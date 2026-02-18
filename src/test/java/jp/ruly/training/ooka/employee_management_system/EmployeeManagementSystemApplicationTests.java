8cc8a0c104ec5d5c2d708af772801d180cff25f8
ruly-ooka <ayana.ruly@outlook.jp>
2026-02-18 10:33:33 +0900

    initial commit

---
+++ src/test/java/jp/ruly/training/ooka/employee_management_system/EmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.ruly.training.ooka.employee_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
